1bfa31cba1f9f62810a22598fa6858c10d73f3c9
Tuur Neckebroeck <tuur.neckebroeck@gmail.com>
2021-07-13 01:25:00 +0200

    Fixed identity visualization

--- models/conditional_cycle_gan_model.py
+++ models/conditional_cycle_gan_model.py
@@ -217,12 +217,16 @@ class ConditionalCycleGANModel(BaseModel):
             # G_A should be identity if real_B is fed: ||G_A(B) - B||
             # self.idt_A = self.netG(self.real_B)
             # self.loss_idt_A = self.criterionIdt(self.idt_A, self.real_B) * lambda_B * lambda_idt
-            self.loss_idt_A = self.criterionIdt(self.fake_A, self.real_B) * lambda_B * lambda_idt
+            color_A = ConditionalCycleGANModel.onehot_encode_colors(self.color_A).cuda()
+            self.idt_A = self.netG(torch.cat([self.real_B, color_A], dim=1))
+            self.loss_idt_A = self.criterionIdt(self.idt_A, self.real_B) * lambda_B * lambda_idt
             
             # G_B should be identity if real_A is fed: ||G_B(A) - A||
             # self.idt_B = self.netG_B(self.real_A)
             # self.loss_idt_B = self.criterionIdt(self.idt_B, self.real_A) * lambda_A * lambda_idt
-            self.loss_idt_B = self.criterionIdt(self.fake_B, self.real_A) * lambda_A * lambda_idt
+            color_B = ConditionalCycleGANModel.onehot_encode_colors(self.color_B).cuda()
+            self.idt_B = self.netG(torch.cat([self.real_A, color_B], dim=1))
+            self.loss_idt_B = self.criterionIdt(self.idt_B, self.real_A) * lambda_A * lambda_idt
         else:
             self.loss_idt_A = 0
             self.loss_idt_B = 0
